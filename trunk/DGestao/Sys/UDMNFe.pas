92bfa2f433982bfa089a4aec3ce794c8f293a6e4
isaque_programmer@yahoo.com.br <isaque_programmer@yahoo.com.br@d8b50c30-55c5-23f6-292f-6240efbc1f6c>
2013-04-11 21:59:46 +0000

    Novos ajustes no cancelamento da NF-e.
    Informação do código do órgão (Código IBGE do Estado) no evento de cancelamento.

--- trunk/DGestao/Sys/UDMNFe.pas
+++ trunk/DGestao/Sys/UDMNFe.pas
@@ -979,6 +979,10 @@ begin
 
       with EventoNFe.Evento.Add do
       begin
+        //  (AC,AL,AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,PB,PR,PE,PI,RJ,RN,RS,RO,RR,SC,SP,SE,TO);
+        //  (12,27,16,13,29,23,53,32,52,21,51,50,31,15,25,41,26,22,33,24,43,11,14,42,35,28,17);
+
+        infEvento.cOrgao     := qryEmitenteEST_COD.AsInteger; // Código IBGE do Estado
         infEvento.chNFe      := qryNFeEmitidaCHAVE.AsString;
         infEvento.CNPJ       := sCNPJEmitente;
         infEvento.dhEvento   := GetDateTimeDB;
