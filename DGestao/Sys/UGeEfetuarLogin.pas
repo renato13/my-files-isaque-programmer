0ed9ce020008568319e5e560eb11a7f3a928b667
isaque_programmer <isaque_programmer@yahoo.com.br>
2013-07-17 12:11:24 +0000

    Ajustes na compitação!

--- DGestao/Sys/UGeEfetuarLogin.pas
+++ DGestao/Sys/UGeEfetuarLogin.pas
@@ -5,7 +5,7 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, DB, IBCustomDataSet, IBQuery, StdCtrls, Buttons,
-  ExtCtrls, dxGDIPlusClasses, UGrPadraoLogin;
+  ExtCtrls, UGrPadraoLogin;
 
 type
   TFrmEfetuarLogin = class(TfrmGrPadraoLogin)
